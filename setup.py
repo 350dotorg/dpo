4ab22add757680c0a18785ee719e3b12b891e36d
Ethan Jucovy <ejucovy@gmail.com>
2012-03-03 14:04:54 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+long_description = ""
+
+setup(name='dpo',
+      version=version,
+      description="Tools to manage PO files without canonical source strings",
+      long_description=long_description,
+      classifiers=[], 
+      keywords='',
+      author='Ethan Jucovy',
+      author_email='ethan.jucovy@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        "polib",
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
